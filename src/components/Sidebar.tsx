7399f3ee165f587d191184ccad1e5bac60e23ca3
koowin42@gmail.com <koowin42@gmail.com>
2023-10-30 22:43:27 +0900

    Implement sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,37 @@
 import { ReactElement } from "react";
+import { Link } from "react-router-dom";
 
-export default function Sidebar(): ReactElement {
-  return <div className="bg-orange-500">This is side bar</div>;
+const links = {
+  history: "/mypage/history",
+  mypost: "/mypage/mypost",
+  edit: "/mypage/edit",
+  withdrawal: "/mypage/withdrawal",
+};
+export default function Sidebar({ path }: { path: string }): ReactElement {
+  return (
+    <div className="mr-4 flex items-start justify-end pt-10">
+      <div className="flex w-32 flex-col gap-3 rounded-xl border py-4 pl-4">
+        <Link to={links.history}>
+          <div className={path === links.history ? "font-bold" : ""}>
+            구매 이력
+          </div>
+        </Link>
+        <Link to={links.mypost}>
+          <div className={path === links.mypost ? "font-bold" : ""}>
+            나의 문의
+          </div>
+        </Link>
+        <Link to={links.edit}>
+          <div className={path === links.edit ? "font-bold" : ""}>
+            내 정보수정
+          </div>
+        </Link>
+        <Link to={links.withdrawal}>
+          <div className={path === links.withdrawal ? "font-bold" : ""}>
+            회원 탈퇴
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,13 +1,20 @@
 import Header from "../components/Header.tsx";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "../components/Sidebar.tsx";
+import { useEffect, useState } from "react";
 
 export default function MyPage() {
+  const [path, setPath] = useState<string>("");
+  const location = useLocation();
+
+  useEffect(() => {
+    setPath(location.pathname);
+  }, [location]);
   return (
     <>
       <Header />
       <div className="grid grid-cols-[1fr_64rem_1fr] justify-center">
-        <Sidebar />
+        <Sidebar path={path} />
         <div className="mt-8 h-screen w-full min-w-max max-w-5xl">
           <Outlet />
         </div>
