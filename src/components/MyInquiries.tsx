c5d53827fe55fe808e31ad6282774978cc7f0c28
koowin42@gmail.com <koowin42@gmail.com>
2023-11-05 20:33:18 +0900

    Rename component

--- src/components/MyInquiries.tsx
+++ src/components/MyInquiries.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 
-export default function MyPost(): ReactElement {
+export default function MyInquiries(): ReactElement {
   return (
     <div>
       <div className="border-b pb-2 pl-4 text-3xl">나의 문의</div>
