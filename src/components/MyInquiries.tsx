cdc109decfafc2d96b947ada3ad7a80dd45ba015
koowin42@gmail.com <koowin42@gmail.com>
2023-11-05 21:01:40 +0900

    Implement the layout of myInquiries

--- src/components/MyInquiries.tsx
+++ src/components/MyInquiries.tsx
@@ -1,9 +1,38 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+import { sampleMyInquiries } from "../sampleData/myInquiry.ts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function MyInquiries(): ReactElement {
+  const [selectedId, setSelectedId] = useState<number>(-1);
   return (
     <div>
       <div className="border-b pb-2 pl-4 text-3xl">나의 문의</div>
+      {sampleMyInquiries.map((inquiry, index) => (
+        <div
+          className={`flex items-center border-b py-2 ${
+            selectedId === index ? "bg-gray-100" : ""
+          }`}
+          key={`inquiry_${index}`}
+          onClick={() =>
+            selectedId !== index ? setSelectedId(index) : setSelectedId(-1)
+          }
+        >
+          <div className="mx-5">{index + 1}</div>
+          <img
+            className="mr-4 h-20 w-20"
+            src={inquiry.thumbnailUrl}
+            alt={`thumbnail_${index}`}
+          />
+          <div className="flex flex-col">
+            <div>{inquiry.inquiry}</div>
+            <div hidden={index !== selectedId} className="ml-5 mt-2">
+              <FontAwesomeIcon className="mr-3" icon={faChevronRight} />
+              {inquiry.answer}
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
