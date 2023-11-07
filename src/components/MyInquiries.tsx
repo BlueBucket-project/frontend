ff1956a7cc8fa8c3f116758cc2df919fdfc7b9c0
koowin42@gmail.com <koowin42@gmail.com>
2023-11-08 00:25:19 +0900

    Fix layout of MyInquiries

--- src/components/MyInquiries.tsx
+++ src/components/MyInquiries.tsx
@@ -5,12 +5,25 @@ import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function MyInquiries(): ReactElement {
   const [selectedId, setSelectedId] = useState<number>(-1);
+
+  const getTitle = (text: string, index: number): string => {
+    if (selectedId === index) {
+      return text;
+    }
+    return text.length > 30 ? text.slice(0, 30) + " ..." : text;
+  };
   return (
-    <div>
+    <div className="max-w-5xl">
       <div className="border-b pb-2 pl-4 text-3xl">나의 문의</div>
+      <div className="grid grid-cols-8 border-b border-t py-4 text-center">
+        <div className="col-span-1">상품</div>
+        <div className="col-span-1">답변상태</div>
+        <div className="col-span-5">제목</div>
+        <div className="col-span-1">작성일</div>
+      </div>
       {sampleMyInquiries.map((inquiry, index) => (
         <div
-          className={`flex items-center border-b py-2 ${
+          className={`flex flex-col ${
             selectedId === index ? "bg-gray-100" : ""
           }`}
           key={`inquiry_${index}`}
@@ -18,19 +31,35 @@ export default function MyInquiries(): ReactElement {
             selectedId !== index ? setSelectedId(index) : setSelectedId(-1)
           }
         >
-          <div className="mx-5">{index + 1}</div>
-          <img
-            className="mr-4 h-20 w-20"
-            src={inquiry.thumbnailUrl}
-            alt={`thumbnail_${index}`}
-          />
-          <div className="flex flex-col">
-            <div>{inquiry.inquiry}</div>
-            <div hidden={index !== selectedId} className="ml-5 mt-2">
-              <FontAwesomeIcon className="mr-3" icon={faChevronRight} />
-              {inquiry.answer}
+          <div className="grid grid-cols-8 items-center border-b py-2">
+            <img
+              className="col-span-1 h-20 w-20 place-self-center"
+              src={inquiry.thumbnailUrl}
+              alt={`thumbnail_${index}`}
+            />
+            <div className="col-span-1 text-center">
+              {inquiry.answer ? "답변완료" : "미답변"}
             </div>
+            <div className="col-span-5 whitespace-pre-line break-all">
+              {getTitle(inquiry.title, index)}
+            </div>
+            <div className="col-span-1 place-self-center">{inquiry.date}</div>
           </div>
+          {inquiry.answer && (
+            <div hidden={index !== selectedId}>
+              <div className="grid grid-cols-8">
+                <div className="col-start-3 col-end-8 my-4 flex">
+                  <FontAwesomeIcon
+                    className="mr-4 mt-1"
+                    icon={faChevronRight}
+                  />
+                  <div className="whitespace-pre-line break-all">
+                    {inquiry.answer}
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       ))}
     </div>
