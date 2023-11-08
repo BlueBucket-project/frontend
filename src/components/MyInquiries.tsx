fa52e4d6d7e7c68e1662836a87acd39f78ec8d37
koowin42@gmail.com <koowin42@gmail.com>
2023-11-09 01:22:13 +0900

    Implement admin/inquires page

--- src/components/MyInquiries.tsx
+++ src/components/MyInquiries.tsx
@@ -6,16 +6,10 @@ import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 export default function MyInquiries(): ReactElement {
   const [selectedId, setSelectedId] = useState<number>(-1);
 
-  const getTitle = (text: string, index: number): string => {
-    if (selectedId === index) {
-      return text;
-    }
-    return text.length > 30 ? text.slice(0, 30) + " ..." : text;
-  };
   return (
     <div className="max-w-5xl">
       <div className="border-b pb-2 pl-4 text-3xl">나의 문의</div>
-      <div className="grid grid-cols-8 border-b border-t py-4 text-center">
+      <div className="grid grid-cols-8 border-b border-t border-black py-4 text-center">
         <div className="col-span-1">상품</div>
         <div className="col-span-1">답변상태</div>
         <div className="col-span-5">제목</div>
@@ -31,7 +25,7 @@ export default function MyInquiries(): ReactElement {
             selectedId !== index ? setSelectedId(index) : setSelectedId(-1)
           }
         >
-          <div className="grid grid-cols-8 items-center border-b py-2">
+          <div className="grid grid-cols-8 items-center border-b border-gray-400 py-2">
             <img
               className="col-span-1 h-20 w-20 place-self-center"
               src={inquiry.thumbnailUrl}
@@ -41,13 +35,13 @@ export default function MyInquiries(): ReactElement {
               {inquiry.answer ? "답변완료" : "미답변"}
             </div>
             <div className="col-span-5 whitespace-pre-line break-all">
-              {getTitle(inquiry.title, index)}
+              {getTitle(inquiry.title, index === selectedId)}
             </div>
             <div className="col-span-1 place-self-center">{inquiry.date}</div>
           </div>
           {inquiry.answer && (
             <div hidden={index !== selectedId}>
-              <div className="grid grid-cols-8">
+              <div className="grid grid-cols-8 border-b border-gray-400">
                 <div className="col-start-3 col-end-8 my-4 flex">
                   <FontAwesomeIcon
                     className="mr-4 mt-1"
@@ -65,3 +59,10 @@ export default function MyInquiries(): ReactElement {
     </div>
   );
 }
+
+const getTitle = (title: string, isSelected: boolean): string => {
+  if (isSelected) {
+    return title;
+  }
+  return title.length > 30 ? title.slice(0, 30) + " ..." : title;
+};
--- src/pages/InquiryList.tsx
+++ src/pages/InquiryList.tsx
@@ -1,5 +1,109 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+import Header from "../components/Header.tsx";
+import { sampleMyInquiries } from "../sampleData/myInquiry.ts";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function InquiryList(): ReactElement {
-  return <div>InquiryList</div>;
+  const [filterUnanswered, setFilterUnanswered] = useState<boolean>(false);
+  const [selectedInquiryIndex, setSelectedInquiryIndex] = useState<number>(-1);
+  return (
+    <>
+      <Header />
+      <div className="mx-auto mt-4 h-screen min-w-max max-w-5xl">
+        <div className="flex max-w-5xl items-end justify-between border-b-2 border-black pb-2 text-3xl">
+          <div>상품 문의</div>
+          <div className="flex gap-2">
+            <input
+              type="checkbox"
+              checked={filterUnanswered}
+              onChange={() => setFilterUnanswered(!filterUnanswered)}
+            />
+            <div className="text-base">답변 미완료만 보기</div>
+          </div>
+        </div>
+        <div className="grid grid-cols-8 border-b border-black text-center">
+          <div className="col-span-1">답변상태</div>
+          <div className="col-span-1">상품</div>
+          <div className="col-span-4">제목</div>
+          <div className="col-span-1">작성자</div>
+          <div className="col-span-1">작성일</div>
+        </div>
+        {sampleMyInquiries.map((inquiry, index) => (
+          <div
+            key={`inquiry_${index}`}
+            className={`${selectedInquiryIndex === index ? "bg-gray-100" : ""}`}
+          >
+            <div className="grid max-w-5xl grid-cols-8 place-items-center border-b border-gray-400 py-2">
+              <div className="col-span-1">
+                {inquiry.answer ? "답변완료" : "미답변"}
+              </div>
+              <img
+                src={inquiry.thumbnailUrl}
+                alt={`thumbnail_${index}`}
+                className="col-span-1 h-20 w-20"
+              />
+              <div
+                className="col-span-4 flex flex-col gap-2 place-self-start self-center justify-self-stretch whitespace-pre-line break-all"
+                onClick={() => {
+                  if (selectedInquiryIndex === index) {
+                    setSelectedInquiryIndex(-1);
+                    return;
+                  }
+                  setSelectedInquiryIndex(index);
+                }}
+              >
+                <div className="font-bold text-blue-600">상품명</div>
+                <div>
+                  {getTitle(inquiry.title, index === selectedInquiryIndex)}
+                </div>
+              </div>
+              <div
+                className="col-span-1 text-center"
+                onClick={() => console.log("HI")}
+              >{`작성자${index + 1}`}</div>
+              <div className="col-span-1 text-center">{inquiry.date}</div>
+            </div>
+            <div hidden={selectedInquiryIndex !== index}>
+              {renderAnswerArea(inquiry.answer)}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
+
+const getTitle = (title: string, isSelected: boolean): string => {
+  if (isSelected) {
+    return title;
+  }
+  return title.length > 30 ? title.slice(0, 30) + " ..." : title;
+};
+
+const renderAnswerArea = (answer: string): ReactElement => {
+  return (
+    <div className="grid max-w-5xl grid-cols-8 border-b border-gray-400 py-2">
+      {answer === "" ? (
+        <div className="col-start-3 col-end-7 flex flex-col gap-2">
+          <textarea
+            name="answer_area"
+            id="answer_area"
+            placeholder="답변 입력"
+            cols={60}
+            rows={5}
+            className="border border-gray-800 px-2 py-2"
+          />
+          <button className="w-32 self-end rounded bg-blue-300">
+            답변하기
+          </button>
+        </div>
+      ) : (
+        <div className="col-start-3 col-end-7 flex whitespace-pre-line break-all">
+          <FontAwesomeIcon className="mr-4 mt-1" icon={faChevronRight} />
+          <div>{answer}</div>
+        </div>
+      )}
+    </div>
+  );
+};
