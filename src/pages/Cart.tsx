31af59794b3d28347ce4028b9d9fb7e30aa8d08a
towerbreaker2200 <69230199+towerbreaker2200@users.noreply.github.com>
2023-11-19 17:08:18 +0900

    장바구니 컴포넌트 재결합

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,7 +3,6 @@ import Header from "../components/Header";
 import { ICartItem } from "../responseTypes";
 import { instanceH } from "../api";
 import { useAppSelector } from "../app/hooks";
-import { CartItem } from "../components/CartItem";
 
 interface ICartItems extends Array<ICartItem> {}
 
@@ -19,16 +18,39 @@ export default function Cart() {
       });
   }, []);
 
+  const cartItemDelete = (item: ICartItem) => {
+    instanceH(accessToken)
+      .post("/cart/items", [
+        {
+          cartId: item.cart.cartId,
+          count: item.count,
+          itemId: item.item.itemId,
+        },
+      ])
+      .then(() => {});
+  };
+
+  const cartItemReserve = (id: number) => {
+    instanceH(accessToken)
+      .post("/cart/orderItems", [
+        {
+          cartItemId: id,
+        },
+      ])
+      .then(() => {});
+  };
+
   const allItemReserve = () => {
-    const allItemId: any[] = [];
+    const allItemId: Record<string, number>[] = [];
     list.map((item) => {
       allItemId.push({ cartItemId: item.cartItemId });
     });
-    instanceH(accessToken)
+    console.log(allItemId);
+    /* instanceH(accessToken)
       .post("/cart/orderItems", allItemId)
       .then((res) => {
         console.log(res);
-      });
+      }); */
   };
 
   return (
@@ -43,7 +65,32 @@ export default function Cart() {
         </div>
         <div className="">
           {list.map((item) => {
-            return <CartItem key={item.cartItemId} item={item} />;
+            return (
+              <div className="flex h-40 w-full items-center justify-between border-b py-8">
+                <div className="h-28 w-28 rounded-lg bg-blue-100 ">이미지</div>
+                <div className="flex-col justify-between ">
+                  <div className="mb-4 w-96">{item.item.itemName}</div>
+                  <div>
+                    {item.item.price
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                  </div>
+                </div>
+                <div>
+                  <div>{item.count}</div>
+                  <div>수정하기</div>
+                </div>
+                <div>{item.count * item.item.price}</div>
+                <div className="flex flex-col items-end">
+                  <button onClick={() => cartItemDelete(item)} className="mb-4">
+                    X
+                  </button>
+                  <button onClick={() => cartItemReserve(item.cartItemId)}>
+                    선택구매예약
+                  </button>
+                </div>
+              </div>
+            );
           })}
         </div>
         <div className="mt-8 w-full">
