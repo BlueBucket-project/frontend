7d4e952dc8a906577e521300bee3c154711113c5
koowin42@gmail.com <koowin42@gmail.com>
2023-11-22 00:49:50 +0900

    Implement history component

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,33 +1,48 @@
-import { ReactElement, useState } from "react";
-import { History, SampleHistory } from "../sampleData/history.ts";
+import { ReactElement, useEffect, useState } from "react";
+import { useAppSelector } from "../app/hooks.ts";
+import { instanceH } from "../api";
+import { useNavigate } from "react-router-dom";
 
 export default function History(): ReactElement {
-  const [histories, _] = useState<History[]>(SampleHistory);
+  const [histories, setHistories] = useState<PurchaseHistory[]>([]);
+  const navigate = useNavigate();
+  const user = useAppSelector((state) => state.user);
+
+  useEffect(() => {
+    instanceH(user.accessToken)
+      .get(`/users/order/${user.memberEmail}`)
+      .then((res) => setHistories(res.data));
+  }, []);
 
   return (
     <div>
-      <div className="border-b pb-2 pl-4 text-3xl">구매 이력</div>
-      <div className="grid grid-cols-2">
-        {histories.map((h: History, i: number) => (
-          <div
-            key={`history_${i}`}
-            className="m-4 flex rounded-xl bg-gray-100 p-5"
-          >
-            <img
-              className="mr-6 h-24 w-24"
-              src={h.thumbnailUrl}
-              alt={i.toString()}
-            />
-            <div className="flex flex-col">
-              <div className="text-sm text-gray-500">{h.date} 구매</div>
-              <div className="text-lg">{h.name}</div>
-              <div className="text-lg font-bold">
-                {(h.price * h.quantity).toLocaleString()}원
-              </div>
-            </div>
-          </div>
-        ))}
+      <div className="border-b-2 border-black pb-2 pl-4 text-3xl">
+        구매 이력
       </div>
+      <div className="grid max-w-5xl grid-cols-6 gap-3 border-b border-gray-400 py-4 pl-10 text-center font-bold">
+        <div className="col-span-3">상품명</div>
+        <div className="col-span-1">가격</div>
+        <div className="col-span-1">수량</div>
+        <div className="col-span-1">등록일</div>
+      </div>
+      {histories.map((history, i) => (
+        <div
+          key={`history_${i}`}
+          className="grid max-w-5xl grid-cols-6 items-center justify-items-center gap-3 border-b border-gray-400 py-4 pl-10"
+        >
+          <div
+            className="col-span-3 justify-self-start hover:cursor-pointer"
+            onClick={() => navigate(`/item/${history.itemId}`)}
+          >
+            {history.item.itemName}
+          </div>
+          <div className="col-span-1">
+            {history.itemPrice.toLocaleString()} 원
+          </div>
+          <div className="col-span-1">{history.itemAmount} 개</div>
+          <div className="col-span-1">{history.orderDate.split("T")[0]}</div>
+        </div>
+      ))}
     </div>
   );
 }
