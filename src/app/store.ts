b9f315cc61d39433b8909a70aa69019ba697f8df
towerbreaker2200 <69230199+towerbreaker2200@users.noreply.github.com>
2023-12-04 11:55:33 +0900

    redux-persist 적용

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+
+const reducers = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
