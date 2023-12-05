de80ff98fb73e1ab538677b324028ee39c585ffd
towerbreaker2200 <69230199+towerbreaker2200@users.noreply.github.com>
2023-12-05 21:43:38 +0900

    상품 문의 content 길이 조정

--- src/components/QNABoard.tsx
+++ src/components/QNABoard.tsx
@@ -42,25 +42,26 @@ export function QNABoard({ item, rerender }: BoardProps) {
         className="block grid  grid-cols-10 items-center border-b border-b-gray-400 py-4 text-center hover:cursor-pointer"
       >
         <div>{item.replyStatus === "REPLY_O" ? "답변완료" : "미답변"}</div>
-        <div className="col-span-7 col-start-2 flex items-center text-ellipsis text-left">
+        <div className="col-span-7 col-start-2 flex items-center text-left">
           {item.boardSecret === "LOCK" ? (
             <>
               <FontAwesomeIcon icon={faLock} className="h-6 w-6" />
               잠긴 문의글입니다.
             </>
           ) : isEditing ? (
-            <div className="w-full">
-              <input
+            <div className="flex w-full items-center">
+              <textarea
+                rows={4}
                 className="w-3/4"
                 value={content}
                 onChange={(e) => setContent(e.target.value)}
               />
-              <button onClick={boardEdit} className="bg-yellow-100 p-2">
+              <button onClick={boardEdit} className="ml-4 bg-yellow-100 p-2">
                 수정완료
               </button>
             </div>
           ) : (
-            item.content
+            <div className="whitespace-pre-line break-all">{item.content}</div>
           )}
           {item.boardSecret === "LOCK" || isEditing ? null : (
             <div className="ml-4 w-48">
