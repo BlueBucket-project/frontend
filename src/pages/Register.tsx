30d24a2d89b1f7c58bc7d91c45142b52994f531a
towerbreaker2200 <69230199+towerbreaker2200@users.noreply.github.com>
2023-12-05 12:38:08 +0900

    회원가입 이메일, 비밀번호, 닉네임 정규식 적용

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -16,6 +16,52 @@ export default function Register(): ReactElement {
   const [addr, setAddr] = useState<string>("");
   const [addrDetail, setAddrDetail] = useState<string>("");
   const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
+  const [mailError, setMailError] = useState<string>("");
+  const [pwError, setPwError] = useState<string>("");
+  const [NNError, setNNError] = useState<string>("");
+
+  const handleValidation = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.id === "id") {
+      setId(e.target.value);
+      const isValidEmail = /^[[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-z]{2,3}/.test(
+        e.target.value,
+      );
+      if (e.target.value === "") {
+        setMailError("");
+      } else if (!isValidEmail) {
+        setMailError("이메일이 올바르지 않습니다.");
+      } else {
+        setMailError("");
+      }
+    } else if (e.target.id === "password") {
+      setPassword(e.target.value);
+      const isValidPassword =
+        /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(
+          e.target.value,
+        );
+      if (e.target.value === "") {
+        setPwError("");
+      } else if (e.target.value.length < 8) {
+        setPwError("비밀번호는 8자 이상이어야 합니다.");
+      } else if (!isValidPassword) {
+        setPwError(
+          "비밀번호는 영어, 숫자, 특수문자를 적어도 1개는 포함하여야 합니다.",
+        );
+      } else {
+        setPwError("");
+      }
+    } else if (e.target.id === "nickName") {
+      setNickName(e.target.value);
+      const isValidNickname = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/.test(e.target.value);
+      if (e.target.value === "") {
+        setNNError("");
+      } else if (!isValidNickname) {
+        setNNError("닉네임은 한글 혹은 영어여야합니다.");
+      } else {
+        setNNError("");
+      }
+    }
+  };
 
   const navigate = useNavigate();
 
@@ -66,22 +112,24 @@ export default function Register(): ReactElement {
               type="text"
               id="id"
               value={id}
-              onChange={(e) => setId(e.target.value)}
+              onChange={(e) => handleValidation(e)}
               placeholder="이메일"
               className="input-base grow border-gray-400 bg-gray-100 pl-3"
             />
           </div>
+          {mailError && <p className="text-base text-red-400">{mailError}</p>}
           <div className="flex items-center gap-3">
             <div className="w-28 text-left">비밀번호</div>
             <input
               type="password"
               id="password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => handleValidation(e)}
               placeholder="비밀번호"
               className="input-base grow border-gray-400 bg-gray-100 pl-3"
             />
           </div>
+          {pwError && <p className="text-base text-red-400">{pwError}</p>}
           <div className="flex items-center gap-3">
             <div className="w-28 text-left">이름</div>
             <input
@@ -99,11 +147,12 @@ export default function Register(): ReactElement {
               type="nickName"
               id="nickName"
               value={nickName}
-              onChange={(e) => setNickName(e.target.value)}
+              onChange={(e) => handleValidation(e)}
               placeholder="닉네임"
               className="input-base grow border-gray-400 bg-gray-100 pl-3"
             />
           </div>
+          {NNError && <p className="text-base text-red-400">{NNError}</p>}
           <div className="flex items-center gap-3">
             <div className="w-28 text-left">우편번호</div>
             <input
